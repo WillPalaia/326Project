5cac13c8816bc30d5ad86989d5311aeb20c673a6
DongpeiZ <david.zhang1459@gmail.com>
2024-11-06 15:02:36 -0500

    started gear rec

--- front-end/source/components/AppControllerComponent/AppControllerComponent.js
+++ front-end/source/components/AppControllerComponent/AppControllerComponent.js
@@ -4,6 +4,7 @@ import { TaskInputComponent } from '../TaskInputComponent/TaskInputComponent.js'
 import { EventHub } from '../../eventhub/EventHub.js';
 import { DynamicSidebarComponent } from '../DynamicSidebarComponent/DynamicSidebarComponent.js';
 import { MainPageComponent } from '../MainPageComponent/MainPageComponent.js';
+import { GearRecComponent } from '../GearRecComponent/GearRecComponent.js';
 
 export class AppControllerComponent {
   #container = null; // Private container for the component
@@ -14,6 +15,7 @@ export class AppControllerComponent {
   #hub = null; // EventHub instance for managing events
   #dynamicSidebarComponent = null;
   #mainPageComponent = null;
+  #GearRecComponent = null;
 
   constructor() {
     this.#hub = EventHub.getInstance();
@@ -22,6 +24,7 @@ export class AppControllerComponent {
     this.#simpleTaskListViewComponent = new SimpleTaskListViewComponent();
     this.#dynamicSidebarComponent = new DynamicSidebarComponent();
     this.#mainPageComponent = new MainPageComponent();
+    this.#GearRecComponent = new GearRecComponent();
   }
 
   // Render the AppController component and return the container
@@ -52,12 +55,24 @@ export class AppControllerComponent {
     this.#container.innerHTML = `
       <div id="viewContainer"></div>
       <button id="switchViewBtn">Switch to Simple View</button>
+      <div id="GearRec"></div>
+      <button id="rec_Button">Gear Recommendation </button>
     `;
   }
 
   // Attaches the necessary event listeners
   #attachEventListeners() {
     const switchViewBtn = this.#container.querySelector('#switchViewBtn');
+    const rec_Button = this.#container.querySelector('#rec_Button')
+
+    rec_Button.addEventListener('click', () => {
+      this.#toggleRecView();
+    })
+
+    this.#hub.subscribe('SwitchToGearRecView', () => {
+      this.#currentView = 'rec'
+      this.#renderCurrentView();
+    })
 
     // Event listener for switching views
     switchViewBtn.addEventListener('click', () => {
@@ -79,6 +94,18 @@ export class AppControllerComponent {
     this.#hub.subscribe("NavigateToPage", (page) => {
       this.#navigateToPage(page);
     });
+
+    this.sub
+  }
+
+  #toggleRecView(){
+    if (this.#currentView === 'main'){
+      this.#currentView = 'rec'
+      this.#hub.publish('SwitchToGearRecView', null);
+    }else {
+      this.#currentView = 'main'
+      this.#hub.publish('SwitchToMainView', null);
+    }
   }
 
   // Toggles the view between main and simple
@@ -126,7 +153,7 @@ export class AppControllerComponent {
     const viewContainer = this.#container.querySelector('#viewContainer');
     viewContainer.innerHTML = ''; // Clear existing content
 
-    // Update the button text based on the current view
+    // Update the button text for the main/simple toggle button
     const switchViewBtn = this.#container.querySelector('#switchViewBtn');
     switchViewBtn.textContent = this.#currentView === 'main' ? 'Switch to Simple View' : 'Switch to Main View';
 
@@ -134,9 +161,13 @@ export class AppControllerComponent {
       // Render the main task list view
       viewContainer.appendChild(this.#taskInputComponent.render());
       viewContainer.appendChild(this.#taskListComponent.render());
-    } else {
+    } else if (this.#currentView === 'simple') {
       // Render the simple task list view
       viewContainer.appendChild(this.#simpleTaskListViewComponent.render());
+    } else if (this.#currentView === 'rec') {
+      // Render the gear recommendation view
+      viewContainer.appendChild(this.#GearRecComponent.render());
+
     }
   }
 }
