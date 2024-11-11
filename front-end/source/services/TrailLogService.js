9bdaeebaaf6d899e0430f1d3650d5147f5856efd
brt427 <brt427@gmail.com>
2024-11-11 13:43:13 -0500

    Created a skeleton TrailLogService.js file based on TaskRepositoryService.js. Wrote documentation describing the goals of the service and how it will be integraetd into the project.

---
+++ front-end/source/services/TrailLogService.js
@@ -0,0 +1,115 @@
+import { Events } from '../eventhub/Events.js';
+import Service from './Service.js';
+
+/* 
+This is a service file that can be used to log data in the browser 
+redgarding the trails that a user adds. It will track the trail name, 
+location, length, and date hiked. 
+*/ 
+
+export class TaskRepositoryService extends Service {
+  constructor() {
+    super();
+    this.dbName = 'taskDB';
+    this.storeName = 'tasks';
+    this.db = null;
+
+    // Initialize the database
+    this.initDB()
+      .then(() => {
+        // Load tasks on initialization
+        this.loadTasksFromDB();
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  async initDB() {
+    return new Promise((resolve, reject) => {
+      const request = indexedDB.open(this.dbName, 1);
+
+      request.onupgradeneeded = event => {
+        const db = event.target.result;
+        db.createObjectStore(this.storeName, {
+          keyPath: 'id',
+          autoIncrement: true,
+        });
+      };
+
+      request.onsuccess = event => {
+        this.db = event.target.result;
+        resolve(this.db);
+      };
+
+      request.onerror = event => {
+        reject('Error initializing IndexedDB');
+      };
+    });
+  }
+
+  async storeTask(taskData) {
+    return new Promise((resolve, reject) => {
+      const transaction = this.db.transaction([this.storeName], 'readwrite');
+      const store = transaction.objectStore(this.storeName);
+      const request = store.add(taskData);
+
+      request.onsuccess = () => {
+        this.publish(Events.StoreTaskSuccess, taskData);
+        resolve('Task stored successfully');
+      };
+
+      request.onerror = () => {
+        this.publish(Events.StoreTaskFailure, taskData);
+        reject('Error storing task: ');
+      };
+    });
+  }
+
+  async loadTasksFromDB() {
+    return new Promise((resolve, reject) => {
+      const transaction = this.db.transaction([this.storeName], 'readonly');
+      const store = transaction.objectStore(this.storeName);
+      const request = store.getAll();
+
+      request.onsuccess = event => {
+        const tasks = event.target.result;
+        tasks.forEach(task => this.publish('NewTask', task));
+        resolve(tasks);
+      };
+
+      request.onerror = () => {
+        this.publish(Events.LoadTasksFailure);
+        reject('Error retrieving tasks');
+      };
+    });
+  }
+
+  async clearTasks() {
+    return new Promise((resolve, reject) => {
+      const transaction = this.db.transaction([this.storeName], 'readwrite');
+      const store = transaction.objectStore(this.storeName);
+      const request = store.clear();
+
+      request.onsuccess = () => {
+        this.publish(Events.UnStoreTasksSuccess);
+        resolve('All tasks cleared');
+      };
+
+      request.onerror = () => {
+        this.publish(Events.UnStoreTasksFailure);
+        reject('Error clearing tasks');
+      };
+    });
+  }
+
+  addSubscriptions() {
+    this.subscribe(Events.StoreTask, data => {
+      this.storeTask(data);
+    });
+
+    this.subscribe(Events.UnStoreTasks, () => {
+      this.clearTasks();
+    });
+  }
+}
