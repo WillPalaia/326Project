00e84ab5cdb24582720811313528aabc415d7c3e
brt427 <brt427@gmail.com>
2024-11-14 18:57:02 -0500

    Implemented basic methods  by filling in outline

--- front-end/source/components/InsightsComponent/InsightsComponent.js
+++ front-end/source/components/InsightsComponent/InsightsComponent.js
@@ -1,13 +1,14 @@
 import { BaseComponent } from '../BaseComponent/BaseComponent.js';
 import { Events } from '../../eventhub/Events.js';
 import { EventHub } from '../../eventhub/EventHub.js';
-import { TrailLogService } from '../services/TrailLogService.js';
+import { TrailLogService } from '../../services/TrailLogService.js';
 
 export class InsightsComponent extends BaseComponent{
     constructor() {
         super();
         this.loadCSS('InsightsComponent');
         this.hub = EventHub.getInstance();
+        this.trailService = new TrailLogService();
         this.trails = [];
 
         //subscribe to new trail events 
@@ -20,19 +21,40 @@ export class InsightsComponent extends BaseComponent{
 
 }
         render(){
+            //create container for various insights
             const container = document.createElement('div');
             container.className = 'insights-container';
 
             this.totalDistanceElem = document.createElement('div');
             this.totalDistanceElement.className = 'total-distance';
 
+            this.trailCountElement = document.createElement('div');
+            this.trailCountElement.className = 'trail-count';
+            
+            //add to container 
+            container.appendChild(this.totalDistanceElement);
+            container.appendChild(this.trailCountElement);
+
+            this.loadTrailData();
         }
 
         async loadTrailData(){
+            try {
+                const trails = await this.trailService.loadTrailsFromDB();
+                this.trails = trails;
+                this.updateInsights();
+            }
+            catch(error){
+                console.error("Error loading trails from DB",error);
+            }
 
         }
 
         updateInsights(){
+            //update insights based on info 
+            const totalDistance = this.trails.reduce((sum,trail)=>sum+trail.distance,0)
+            this.totalDistanceElement.textContent = `Total Mileage Hiked: ${totalDistance.toFixed(1)} miles`;
 
+            this.trailCountElement.textContent = `Number of Trails Completed: ${this.trails.length}`;
         }
 }
