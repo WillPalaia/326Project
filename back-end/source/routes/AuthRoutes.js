e7db622a52580f6a92ec065a5ade27658ae0d93f
brt427 <brt427@gmail.com>
2024-12-05 10:32:34 -0500

    <Account Deletion Feature> <#81> <Modified AuthRoutes.js to include a route for account deletion. Added TODOs to implement login and logout routes for clarity.>

--- back-end/source/routes/AuthRoutes.js
+++ back-end/source/routes/AuthRoutes.js
@@ -12,7 +12,13 @@ import { isAuthenticated, authorizeRole } from "../auth/middleware.js";
 
 const router = express.Router();
 
+//Registration Route
 router.post("/register", register);
+
+//TODO: Add routes for login and logout
+
+//Deletion Route
+router.delete("/delete", deleteAccount);
 
 
 export default router;
