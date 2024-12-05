4a7ce3afdfef19c1748d1719e0fe9934f28e6a1a
brt427 <brt427@gmail.com>
2024-12-05 09:36:59 -0500

    <Auth> <#80> <Added header, buttons for register, login, logout, and fields for input entry. Added TODOs for Will H. >

--- front-end/source/components/LoginPageComponent/LoginPageComponent.js
+++ front-end/source/components/LoginPageComponent/LoginPageComponent.js
@@ -18,6 +18,46 @@ export class LoginPageComponent extends BaseComponent {
     } else {
       container.innerHTML = ''; // Clear any previous content in the container
     }
+    
+    //Title for Auth
+    const title = document.createElement("h1");
+    title.textContent = "TrailSafe Login";
+    container.appendChild(title);
+
+    //Enter Username
+    const usernameInput = document.createElement("input");
+    usernameInput.type = "text";
+    usernameInput.id = "username";
+    usernameInput.placeholder = "Username";
+    container.appendChild(usernameInput);
+
+    //Enter Password
+    const passwordInput = document.createElement("input");
+    passwordInput.type = "password";
+    passwordInput.id = "password";
+    passwordInput.placeholder = "Password";
+    container.appendChild(passwordInput);
+
+    //Register Button
+    const registerBtn = document.createElement("button");
+    registerBtn.id = "registerBtn";
+    registerBtn.className = "component-box";
+    container.appendChild(registerBtn);
+
+    //Login Button
+    const loginBtn = document.createElement("button");
+    loginBtn.id = "loginBtn";
+    loginBtn.className = "component-box";
+    container.appendChild(loginBtn);
+
+    //Logout Button 
+    const logoutBtn = document.createElement("button");
+    logoutBtn.id = "logoutBtn";
+    logoutBtn.className = "component-box";
+    container.appendChild(logoutBtn);
+
+    //TODO: Add Google Login Button (will)
+    //TODO: Add Admin, add forgot password, add delete button?
 
     return container;
   }
@@ -27,11 +67,11 @@ export class LoginPageComponent extends BaseComponent {
   }
 
   async login() {
-
+  //TODO: For Will
   }
   
   async logout() {
-
+    //TODO: For Will
   }
 
   
