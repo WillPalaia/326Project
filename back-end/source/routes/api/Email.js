acfc81dd2b6947ae5a7de89eb36f0bd45b0e5f07
Tri Than <thantrongtri3@gmail.com>
2024-12-10 10:20:26 -0500

    Current Trip: upload Email route

---
+++ back-end/source/routes/api/Email.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import emailjs from '@emailjs/nodejs';
+const router = express.Router();
+
+// send email in case of emergency
+router.post('/api/email', async (req, res) => {
+  const { name, from, to, contacts } = {
+    name: 'Tri',
+    from: 'hehe',
+    to: 'haha',
+    contacts: ['thantrongtri93@gmail.com'],
+  };
+  let sent_anyone = false;
+  for (let contact of contacts) {
+    emailjs.send(
+      'service_nwx8p7m',
+      'template_70jcvv6',
+      {
+        name: 'John',
+        from: from,
+        to: to,
+        to_user: contact,
+      },
+      {
+        publicKey: 'P1HrsVxOKKRMGJrXo',
+        blockHeadless: true,
+      }
+    );
+  }
+});
+
+export default router;
